1fdd1956994925955bfae771594fbf0a69e5d737
Antonino Cangialosi <ninocangialosi@yahoo.it>
2022-10-21 10:28:09 +0200

    Push last progress

--- producer/producer/main.py
+++ producer/producer/main.py
@@ -1,12 +1,12 @@
 import asyncio
+import logging
 import json
-from typing import Callable, List, Union
+from typing import List, Union
 from kafka3 import KafkaProducer
-# import requests
 import websockets
 
 BITSTAMP_URL = "https://www.bitstamp.net/s/webapp/examples/live_orders_v2.html"
-CHANNEL = "live_orders_v2"
+CHANNEL = "live_orders_btcusd"
 SUBSCRIPTION_JSON = json.dumps({
     "event": "bts:subscribe",
     "data": {
@@ -14,38 +14,35 @@ SUBSCRIPTION_JSON = json.dumps({
     }
 })
 BITSTAMP_WS_URL = "wss://ws.bitstamp.net/"
+KAFKA_URL = "kafka:9092"
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def create_producer(servers: Union[List[str], str] = None) -> KafkaProducer:
-    """ Return istance of a KafkaProducer, with standard server address.
-    To override starndard url or pass multiple servers, use the argument `servers`"""
+    """ return istance of a kafkaproducer, with standard server address.
+    To override starndard url or pass multiple servers,
+     use the argument `servers`"""
     boot_servers = servers if servers else "localhost:9092"
     return KafkaProducer(bootstrap_servers=boot_servers)
 
 
-async def get_order_stream():
-    """ Connect to Bitstamp web-socket and get the stream"""
-    async with websockets.connect(BITSTAMP_WS_URL) as wbs:
-        await wbs.send(SUBSCRIPTION_JSON)
-        res = await wbs.recv()
-        return res
-
-
-async def timed_execution(func: Callable, time: int):
-    """ Run an async `func` for `time` seconds. """
-    await asyncio.sleep(time)
-    result = await asyncio.get_event_loop().run_in_executor(None, func)
-    return result
-
-
 async def main():
-    # res = requests.get(BITSTAMP_URL)
-    # print(res.text)
-    # print(json.dumps(res.json()))
-    stream = asyncio.get_event_loop()\
-        .run_until_complete(
-        timed_execution(get_order_stream, 30))
-    print(stream)
+    """ Connect to Bitstamp web-socket and get the stream"""
+    logging.debug("Connecting to message broker.")
+    producer = create_producer(KAFKA_URL)
+    logging.debug("Kafka connected")
+    async with websockets.connect(BITSTAMP_WS_URL) as stream:
+        await stream.send(SUBSCRIPTION_JSON)
+        while True:
+            event = await stream.recv()
+            logging.debug(f"Received event from websocket: {event}")
+            logging.debug("Sending received event")
+            producer.send(
+                topic="bitstamp",
+                value=event,
+            )
+            logging.debug(f"Event sent to kafka successfully!")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
